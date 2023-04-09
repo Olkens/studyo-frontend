b7d9d81f578b6f279f37a1981b6b05f8dce2e695
“Olkens” <olkusz.michal@gmail.com>
2023-04-09 20:52:42 +0200

    S-2 | added working modal to single task + some fiexes

---
+++ components/notes/SingleNote.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div>TODO: TITLE</div>
+        <div><p>TODO: input do notatki</p><input type='text'></div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
