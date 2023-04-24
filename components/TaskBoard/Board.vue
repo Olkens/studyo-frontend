b4b5c4ece8bc4847a503899c3df4a3fe41bf841e
“Olkens” <olkusz.michal@gmail.com>
2023-04-24 22:10:30 +0200

    added working featch from db for taskgroups

--- components/TaskBoard/Board.vue
+++ components/TaskBoard/Board.vue
@@ -1,57 +1,35 @@
 <template>
-  <div class="flex items-start overflow-x-auto gap-4">
-    <draggable
-      v-model="taskGroup"
-      group="columns"
-      item-key="id"
-      class="flex gap-4 items-start"
-      :animation="250"
-      handle=".drag-handle"
-    >
-      <template #item="{ element: column }: { element: Column }">
-        <div class="bg-gray-200 column p-5 rounded min-w-[250px]">
-        <button @click="log()">sdg</button>
-        <div v-for="t in groupsTasks">{{ t }}</div>
-          <header class="flex gap-2 mb-4 font-bold">
-            <DragHandle />
-            <input
-              class="title-input bg-transparent focus:bg-white rounded px-1 w-4/5"
-              @keyup.enter="($event.target as HTMLInputElement).blur()"
-              type="text"
-              v-model="column.title"
-            />
-          </header>
+    <div class="flex items-start overflow-x-auto gap-4">
+        <draggable v-model="taskGroup" group="columns" item-key="id" class="flex gap-4 items-start" :animation="250"
+            handle=".drag-handle">
+            <template #item="{ element: column }: { element: Column }">
+                <div class="bg-gray-200 column p-5 rounded min-w-[250px]">
+                    <header class="flex gap-2 mb-4 font-bold">
+                        <DragHandle />
+                        <input class="title-input bg-transparent focus:bg-white rounded px-1 w-4/5"
+                            @keyup.enter=" ($event.target as HTMLInputElement).blur() " type="text" v-model=" column.title " />
+                    </header>
 
-          <draggable
-            v-model="groupsTasks"
-            :group="{ name: 'tasks', pull: alt ? 'clone' : true }"
-            item-key="id"
-            :animation="250"
-            handle=".drag-handle"
-          >
-            <template #item="{ element: task }: { element: Task }">
-              <div class="flex gap-2">
-                <TaskBoardTask
-                  :task="task"
-                  @delete="column.tasks = column.tasks.filter((t) => t.id !== $event)"
-                />
-              </div>
+                    <draggable v-model=" column.tasks " :group=" { name: 'tasks', pull: alt ? 'clone' : true } " item-key="id"
+                        :animation=" 250 " handle=".drag-handle">
+                        <template #item=" { element: task }: { element: Task } ">
+                            <div class="flex gap-2">
+                                <TaskBoardTask :task=" task "
+                                    @delete=" column.tasks = column.tasks.filter((t) => t.id !== $event) " />
+                            </div>
+                        </template>
+                    </draggable>
+
+                    <footer>
+                        <TaskBoardNewTask @add=" createNewTask($event) " :taskGroupId=" taskGroup.id " />
+                    </footer>
+                </div>
             </template>
-          </draggable>
-
-          <footer>
-            <TaskBoardNewTask @add="createNewTask($event)" :taskGroupId="taskGroup.id" />
-          </footer>
-        </div>
-      </template>
-    </draggable>
-    <button
-      @click="createColumn"
-      class="bg-gray-200 whitespace-nowrap p-2 rounded opacity-50"
-    >
-      + Add Another Column
-    </button>
-  </div>
+        </draggable>
+        <button @click=" createColumn " class="bg-gray-200 whitespace-nowrap p-2 rounded opacity-50">
+            + Add Another Column
+        </button>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -62,22 +40,26 @@ import { useTaskStore } from "../../store/taskStore";
 import { useTaskGroupStore } from "../../store/taskGroupStore";
 
 const taskStore = useTaskStore();
-const taskGroupStore = useTaskGroupStore();
+
+const tasks = ref([])
 
 
-const { data: taskGroup } = await useFetch("/api/taskGroup/taskGroup");
-const { data: getTasks } = await useFetch("/api/getTasks")
+const { data: taskGroups } = await useFetch(() => "/api/taskGroup/taskGroup" )
+const { data: getTasks } = useFetch(`/api/getTasks`)
 
-let groupsTasks = ref(getTasks)
+const taskGroup = ref(taskGroups)
+console.log(taskGroups.tasks)
+
+let groupsTasks = ref(taskGroup.tasks)
 
 
 async function createNewTask(task: Task) {
-  taskStore.addNewTask(task);
-  console.log("eloo");
+    taskStore.addNewTask(task);
+    console.log("eloo");
 }
 
 function log() {
-  console.log(groupsTasks);
+    console.log(taskGroups);
 }
 
 // const columns = ref<Column[]>([
@@ -112,17 +94,17 @@ function log() {
 const alt = useKeyModifier("Alt");
 
 function createColumn() {
-  const column: Column = {
-    id: nanoid(),
-    title: "",
-    tasks: [],
-  };
-  //   columns.value.push(column);
-  nextTick(() => {
-    (document.querySelector(
-      ".column:last-of-type .title-input"
-    ) as HTMLInputElement).focus();
-  });
+    const column: Column = {
+        id: nanoid(),
+        title: "",
+        tasks: [],
+    };
+    //   columns.value.push(column);
+    nextTick(() => {
+        (document.querySelector(
+            ".column:last-of-type .title-input"
+        ) as HTMLInputElement).focus();
+    });
 }
 </script>
 
--- server/api/taskGroup/taskGroup.ts
+++ server/api/taskGroup/taskGroup.ts
@@ -1,7 +1,26 @@
+import { TaskGroup } from './../../../types/index';
 import prisma from '~/prisma/client'
 
 export default defineEventHandler(async (event) => {
 
-    return await prisma.taskGroup.findMany()
+    const taskGroup = await prisma.taskGroup.findMany({
+        where: {
+            enabled: true
+        },
+        include: { tasks: true }
+    })
 
+    // const tasks: unknown[] = []
+    // for (let i = 0; i < taskGroup.length; i++) {
+    //     const data = await prisma.task.findMany({
+    //         where: {
+    //             taskGroupId: taskGroup[i].id
+    //         }
+    //     })
+    //     // console.log(data)
+    //     tasks.push(data)
+    // }
+    // console.log(tasks)
+    console.log(taskGroup)
+    return taskGroup
 })
